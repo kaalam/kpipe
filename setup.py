4c3d19524c75b93c029307af9e5176b0ea6b2375
kaalam <kaalam@kaalam.ai>
2021-05-10 13:13:36 +0200

    Renames the whole shabang (pype failed even if not used)

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-	name		 = 'pype',
-	packages	 = ['pype'],
+	name		 = 'kpype',
+	packages	 = ['kpype'],
 	version		 = '0.1',
 	license		 = 'MIT',
 	description  = 'A one-file-script named `pype` that runs only-if-necessary the scripts contained in a folder named ./scripts.',
 	author		 = 'kaalam',
 	author_email = 'kaalam@kaalam.ai',
-	url			 = 'https://github.com/kaalam/pype',
-	download_url = 'https://github.com/kaalam/pype/dist/pype-0.1.tar.gz',
+	url			 = 'https://github.com/kaalam/kpype',
+	download_url = 'https://github.com/kaalam/kpype/dist/kpype-0.1.tar.gz',
 	keywords	 = ['utilities', 'pipeline', 'etl'],
 	classifiers  = [
 		'Development Status :: 3 - Alpha',
