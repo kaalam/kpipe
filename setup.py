216f58e25e0893612e1564413c23d5c67388da13
kaalam <kaalam@kaalam.ai>
2021-05-11 16:26:05 +0200

    Places version in one place

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from distutils.core import setup
 
+VERSION = '0.1.1'
+
 setup(
 	name		 = 'kpipe',
 	packages	 = ['kpipe'],
-	version		 = '0.1.3',
+	version		 = VERSION,
 	license		 = 'MIT',
 	description  = 'A one-file-script named `kpipe` that runs only-if-necessary the scripts contained in a folder named ./scripts.',
 	author		 = 'kaalam',
 	author_email = 'kaalam@kaalam.ai',
 	url			 = 'https://github.com/kaalam/kpipe',
-	download_url = 'https://github.com/kaalam/kpipe/dist/kpipe-0.1.tar.gz',
+	download_url = 'https://github.com/kaalam/kpipe/dist/kpipe-%s.tar.gz' % VERSION,
 	keywords	 = ['utilities', 'pipeline', 'etl'],
 	scripts		 = ['bin/kpipe'],
 	classifiers  = [
